cad931223bb8a85f625a5fe8bd689a42dcb2f0d3
Viktor Prilepin <vicpride@gmail.com>
2019-02-20 07:02:58 +0700

    20/02/19

---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<app-stock v-for="(stock, index) in stocks" :stock="stock" :key="index"></app-stock>
+	</div>
+</template>
+
+<script>
+import Stock from "./Stock.vue";
+
+export default {
+	computed: {
+		stocks() {
+			return this.$store.getters.stocks;
+		}
+	},
+	components: {
+		appStock: Stock
+	}
+};
+</script>

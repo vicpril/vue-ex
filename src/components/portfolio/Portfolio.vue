9ada297da46957c846a54f034c9d9e686d289e7e
Viktor Prilepin <vicpride@gmail.com>
2019-02-21 07:07:29 +0700

    21/02/19

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -1,3 +1,22 @@
 <template>
-    <h1>The Portfolio/Portfolio Component</h1>
+	<div>
+		<app-stock v-for="(stock, index) in stocks" :stock="stock" :key="index"></app-stock>
+	</div>
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+import Stock from "./Stock.vue";
+
+export default {
+	computed: {
+		...mapGetters({
+			stocks: "stockPortfolio"
+		})
+	},
+	components: {
+		appStock: Stock
+	}
+};
+</script>
+
--- src/store/modules/stocks.js
+++ src/store/modules/stocks.js
@@ -1,37 +1,37 @@
 import stocks from "../../data/stocks";
 
 const state = {
-  stocks: []
+    stocks: []
 };
 
 const mutations = {
-  SET_STOCKS(state, stocks) {
-    state.stocks = stocks;
-  },
-  RND_STOCKS(state) {}
+    SET_STOCKS(state, stocks) {
+        state.stocks = stocks;
+    },
+    RND_STOCKS(state) {}
 };
 
 const actions = {
-  buyStock: ({ commit }, order) => {
-    commit();
-  },
-  initStocks: ({ commit }) => {
-    commit("SET_STOCKS", stocks);
-  },
-  randomizeStocks: ({ commit }) => {
-    commit("RND_STOCKS");
-  }
+    buyStock: ({ commit }, order) => {
+        commit("BUY_STOCK", order);
+    },
+    initStocks: ({ commit }) => {
+        commit("SET_STOCKS", stocks);
+    },
+    randomizeStocks: ({ commit }) => {
+        commit("RND_STOCKS");
+    }
 };
 
 const getters = {
-  stocks: state => {
-    return state.stocks;
-  }
+    stocks: state => {
+        return state.stocks;
+    }
 };
 
 export default {
-  state,
-  mutations,
-  actions,
-  getters
+    state,
+    mutations,
+    actions,
+    getters
 };
